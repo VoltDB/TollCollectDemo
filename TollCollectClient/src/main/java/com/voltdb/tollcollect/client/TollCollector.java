07b896c0b7f146ba15831f28e9e5b3adb298ef53
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2025-01-30 12:00:48 +0100

    Split class into two classes, one handling the saga, the oner one handling conneciton stuff.

--- TollCollectClient/src/main/java/com/voltdb/tollcollect/client/TollCollector.java
+++ TollCollectClient/src/main/java/com/voltdb/tollcollect/client/TollCollector.java
@@ -1,10 +1,6 @@
 package com.voltdb.tollcollect.client;
 
 import org.voltdb.VoltTable;
-import org.voltdb.client.Client2;
-import org.voltdb.client.Client2Config;
-import org.voltdb.client.ClientFactory;
-import org.voltdb.client.ClientResponse;
 import org.voltdb.client.ProcCallException;
 
 import java.io.Console;
@@ -13,28 +9,17 @@ import java.math.BigDecimal;
 import java.util.NoSuchElementException;
 
 
-public class TollCollector implements AutoCloseable {
+public class TollCollector {
 
     private static final String DEFAULT_VOLT_HOST = "localhost";
     private static final String DEFAULT_VOLT_PORT = "21212";
-
-    private final String servers;
-    private final Client2 client;
-
-    public TollCollector(String servers) {
-        this.servers = servers;
-
-        // Create a client instance with default configuration values
-        Client2Config config = new Client2Config();
-        client = ClientFactory.createClient(config);
-    }
 
     public static void main(String[] args) {
         Console console = System.console();
         String server = getServerFromUser(console);
 
         // Instantiate object to handle Volt connection
-        try (TollCollector tc = new TollCollector(server)) {
+        try (TollCollectorDbClient tc = new TollCollectorDbClient(server)) {
             // Connect to Volt
             try {
                 tc.connectToVolt();
@@ -123,47 +108,5 @@ public class TollCollector implements AutoCloseable {
             servers = connectionInput;
         }
         return servers;
-    }
-
-    // Method to establish connection with Volt using Client2
-    void connectToVolt() throws IOException {
-        // Connect to Volt using a string for connection details and synchronous completion
-        client.connectSync(servers);
-        System.out.println("Connected to Volt.\n");
-    }
-
-    @Override
-    public void close() {
-        client.close();
-    }
-
-    // Method used to process plate
-    void processScanRow(long scanTimestamp, String location, String lane, String plateNum, String vehicleClass)
-            throws IOException, ProcCallException {
-        System.out.println("Looking up plate... Calculating toll... Writing to scan log... ");
-        // Call ProcessPlate procedure with synchronous completion and return response into ClientResponse object
-        callProcedureSynchronously("ProcessPlate", scanTimestamp, location, lane, plateNum, vehicleClass);
-    }
-
-    // Method used to call AdHoc SQL and return query result as VoltTable
-    VoltTable callAdHocSQL(Object... arguments) throws IOException, ProcCallException {
-        // Call AdHoc procedure by specifying "@AdHoc" as first parameter, followed by string containing SQL code and parameters
-        return client.callProcedureSync("@AdHoc", arguments).getResults()[0];
-    }
-
-    // Method used to charge account
-    void chargeRow(long scanId, long scanTimestamp, String location, String lane, String plateNum, int accountId, BigDecimal tollAmount, String tollReason)
-            throws IOException, ProcCallException {
-        System.out.println("Checking balance... Charging account... Writing to account log... ");
-        // Call ChargeAccount procedure with synchronous completion and return response into ClientResponse object
-        callProcedureSynchronously("ChargeAccount", scanId, scanTimestamp, location, lane, plateNum, accountId, tollAmount, tollReason);
-    }
-
-    private void callProcedureSynchronously(String procedure, Object... arguments) throws IOException, ProcCallException {
-        ClientResponse chargeAccountResponse = client.callProcedureSync(procedure, arguments);
-        // Report transaction performance metrics available in ClientResponse object
-        System.out.printf("Estimated Database Transaction Time: %.3fs | End-to-end Transaction Time: %.3fs%n",
-                (chargeAccountResponse.getClusterRoundtrip() / 1000.0),
-                (chargeAccountResponse.getClientRoundtrip() / 1000.0));
     }
 }
---
+++ TollCollectClient/src/main/java/com/voltdb/tollcollect/client/TollCollectorDbClient.java
@@ -0,0 +1,68 @@
+package com.voltdb.tollcollect.client;
+
+import org.voltdb.VoltTable;
+import org.voltdb.client.Client2;
+import org.voltdb.client.Client2Config;
+import org.voltdb.client.ClientFactory;
+import org.voltdb.client.ClientResponse;
+import org.voltdb.client.ProcCallException;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+
+public class TollCollectorDbClient implements AutoCloseable {
+
+    private final String servers;
+    private final Client2 client;
+
+    public TollCollectorDbClient(String servers) {
+        this.servers = servers;
+
+        // Create a client instance with default configuration values
+        Client2Config config = new Client2Config();
+        client = ClientFactory.createClient(config);
+    }
+
+    // Method to establish connection with Volt using Client2
+    void connectToVolt() throws IOException {
+        // Connect to Volt using a string for connection details and synchronous completion
+        client.connectSync(servers);
+        System.out.println("Connected to Volt.\n");
+    }
+
+    @Override
+    public void close() {
+        client.close();
+    }
+
+    // Method used to process plate
+    void processScanRow(long scanTimestamp, String location, String lane, String plateNum, String vehicleClass)
+            throws IOException, ProcCallException {
+        System.out.println("Looking up plate... Calculating toll... Writing to scan log... ");
+        // Call ProcessPlate procedure with synchronous completion and return response into ClientResponse object
+        callProcedureSynchronously("ProcessPlate", scanTimestamp, location, lane, plateNum, vehicleClass);
+    }
+
+    // Method used to call AdHoc SQL and return query result as VoltTable
+    VoltTable callAdHocSQL(Object... arguments) throws IOException, ProcCallException {
+        // Call AdHoc procedure by specifying "@AdHoc" as first parameter, followed by string containing SQL code and parameters
+        return client.callProcedureSync("@AdHoc", arguments).getResults()[0];
+    }
+
+    // Method used to charge account
+    void chargeRow(long scanId, long scanTimestamp, String location, String lane, String plateNum, int accountId, BigDecimal tollAmount, String tollReason)
+            throws IOException, ProcCallException {
+        System.out.println("Checking balance... Charging account... Writing to account log... ");
+        // Call ChargeAccount procedure with synchronous completion and return response into ClientResponse object
+        callProcedureSynchronously("ChargeAccount", scanId, scanTimestamp, location, lane, plateNum, accountId, tollAmount, tollReason);
+    }
+
+    private void callProcedureSynchronously(String procedure, Object... arguments) throws IOException, ProcCallException {
+        ClientResponse chargeAccountResponse = client.callProcedureSync(procedure, arguments);
+        // Report transaction performance metrics available in ClientResponse object
+        System.out.printf("Estimated Database Transaction Time: %.3fs | End-to-end Transaction Time: %.3fs%n",
+                (chargeAccountResponse.getClusterRoundtrip() / 1000.0),
+                (chargeAccountResponse.getClientRoundtrip() / 1000.0));
+    }
+}
